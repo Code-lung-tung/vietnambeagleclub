7d3b3e18f824096a2b0d56793dcb9a4ca919e8c8
nt phuong <thaiphuongit25@gmail.com>
2020-02-22 10:36:53 +0700

    dsf

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/search', to: 'search#index'
-  get '/home', to: 'home#index'
+  get '/', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/tinymce_assets' => 'tinymce_assets#create'
 end
