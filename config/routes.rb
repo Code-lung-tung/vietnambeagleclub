0537905b60187affd2351f2700523abcb17453f0
nt phuong <thaiphuongit25@gmail.com>
2020-02-23 20:09:44 +0700

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/search', to: 'search#index'
-  get '/', to: 'home#index'
+  get '/bai_viet_moi', to: 'articles#index'
+  get '/:slug', to: 'home#show'
+  get '/portfolio/:id', to: 'dogs#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/tinymce_assets' => 'tinymce_assets#create'
 end
