6a1f4a8bf4736fcaafc27877ca5ae233477f28ba
nt phuong <thaiphuongit25@gmail.com>
2020-03-01 06:36:17 +0700

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/search', to: 'search#index'
-  get '/bai_viet_moi', to: 'articles#index'
+  get '/bai-viet-moi', to: 'articles#index'
+
   get '/:slug', to: 'home#show'
   get '/portfolio/:id', to: 'dogs#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
