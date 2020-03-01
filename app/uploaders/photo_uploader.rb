f0c0f3851978666bcc2ef6a5669452c007c894d5
Duy Tang <duyth1993@gmail.com>
2020-03-01 10:51:04 +0700

    Add cloudinarty

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,6 +3,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  include Cloudinary::CarrierWave if Rails.env.production?
+
   # Choose what kind of storage to use for this uploader:
   storage :file if Rails.env.development? || Rails.env.test?
   # storage :fog
